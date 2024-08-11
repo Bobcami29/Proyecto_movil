73910991476c53a109ecd10329327ec3aa7c464c
BobCami29 <camilaalejandra2567@gmail.com>
2024-08-10 22:26:51 -0500

    chore: Modified config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://cami:aJgwzG0CWYPjuWIPxkWlyHsN7d5S64tm@dpg-cqs26l3qf0us738skfgg-a/calendario_cbix')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
